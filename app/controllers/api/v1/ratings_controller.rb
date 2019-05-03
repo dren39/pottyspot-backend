19b8f2bb181a30f2f4c867cca6193bcf1075dc15
dren39 <dren391@gmail.com>
2019-05-02 20:50:12 -0400

    authorization error fixed, set up Rating backend

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::RatingsController < ApplicationController
+
+  def create
+    # @ratings = []
+    # byebug
+    @rating = Rating.create(value: params[:value], user_id: params[:user_id], toilet_id: params[:toilet_id])
+    @ratings = Rating.select do |rating|
+      rating.toilet_id == @rating.toilet_id
+    end
+    render json: @ratings
+
+  end
 end
